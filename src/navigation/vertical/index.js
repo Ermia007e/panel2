9a49245a6560918db98d424af24836e42ed605b5
melikabarzin <melikabarzin58@gmail.com>
2025-05-14 16:34:22 +0330

    ,

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,30 +1,33 @@
 import { Mail, Home, Airplay, Circle } from "react-feather";
+import news from "./news";
 
-export default [
-  {
-    id: "home",
-    title: "Home",
-    icon: <Home size={20} />,
-    navLink: "/home",
-  },
-  {
-    id: "secondPage",
-    title: "Second Page",
-    icon: <Mail size={20} />,
-    navLink: "/second-page",
-  },
-  {
-    id: "smaplePage",
-    title: "Sample Page",
-    icon: <Airplay size={20} />,
-    // navLink: "/sample",
-    children: [
-      {
-        id: "invoiceList",
-        title: "List",
-        icon: <Circle size={12} />,
-        navLink: "/apps/invoice/list",
-      },
-    ],
-  },
-];
+// export default [
+//   {
+//     id: "home",
+//     title: "Home",
+//     icon: <Home size={20} />,
+//     navLink: "/home",
+//   },
+//   {
+//     id: "secondPage",
+//     title: "Second Page",
+//     icon: <Mail size={20} />,
+//     navLink: "/second-page",
+//   },
+//   {
+//     id: "smaplePage",
+//     title: "Sample Page",
+//     icon: <Airplay size={20} />,
+//     // navLink: "/sample",
+//     children: [
+//       {
+//         id: "invoiceList",
+//         title: "List",
+//         icon: <Circle size={12} />,
+//         navLink: "/apps/invoice/list",
+//       },
+//     ],
+//   },
+//   // [...news]
+// ];
+export default [...news];
